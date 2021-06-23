8b2bcb4ecb4cfea713569026186cd2d506d49e74
kei-i-wa <kei.kumarg@gmail.com>
2021-06-23 09:24:53 +0000

    Initial Commit

---
+++ compare.rb
@@ -0,0 +1,15 @@
+# https://paiza.jp/works/mondai/skillcheck_sample/diff_str
+a = gets.chomp
+b = gets.chomp
+
+if a==b
+    puts"OK"
+else 
+    puts"NG"
+end
+
+# 模範？解答
+a = gets.chomp
+b = gets.chomp
+
+puts a == b ? 'OK' : 'NG'
